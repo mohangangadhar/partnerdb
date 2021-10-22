53a69451d104e21d7f438fae1177c949f8fcaa6a
imbhavaniprasad <bhavaniprasadsmart@gmail.com>
2021-10-22 23:46:39 +0530

    first

--- src/Wallet/WalletInfo.js
+++ src/Wallet/WalletInfo.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import Paper from "@material-ui/core/Paper";
-import {Button, FormLabel, Grid, TableCell, TextField, Typography} from "@material-ui/core";
+import {Button, FormLabel, FormControl,Grid, MenuItem, TableCell, TextField, Typography, Select, Divider} from "@material-ui/core";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
@@ -13,6 +13,10 @@ class WalletInfo extends Component {
     constructor(props) {
         super(props);
         this.updateBalance = this.updateBalance.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleChangeWallet = this.handleChangeWallet.bind(this);
+        this.handleChangeDescription = this.handleChangeDescription.bind(this);
+        this.proceedTransaction = this.proceedTransaction.bind(this);
         this.state = {
             data: this.props.data,
             wallet: {
@@ -36,7 +40,53 @@ class WalletInfo extends Component {
     handleChange(event) {
         this.setState({balance: event.target.value})
     }
-
+         handleChangeWallet(event){
+            const name = event.target.name;
+            const value = event.target.value;
+             this.setState({
+             wallet : { 
+                     ...this.state.wallet,
+                     [name] : value,
+                    }
+             }
+             );
+             console.log(this.state.wallet);
+         }
+         handleChangeDescription(event){
+            this.setState({
+                wallet : { 
+                    ...this.state.wallet,
+                    meta : {
+                        ...this.state.wallet.meta,
+                        description : event.target.value,
+                    }
+                }
+            })
+                console.log(this.state.wallet.meta);
+         }
+         proceedTransaction(){
+                    this.state.data.wallet = this.state.wallet;
+                    this.state.data.wallet.meta = this.state.wallet.meta;
+                    console.log(this.state.data);
+                    fetch(`https://www.alfanzo.com:443/wallet/${this.state.wallet.walletId}`,{
+                        method: 'POST', // or 'PUT'
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Access-Control-Allow-Origin': '*',
+                            'Access-Control-Allow-Credentials': 'true'
+                        },
+                        body: JSON.stringify(this.state.data.wallet),
+                    })
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log('Success:', data);
+                            NotificationManager.success('You changes have been updated!', 'Successful!', 1000);
+                        })
+                        .catch((error) => {
+                            console.error('Error:', error);
+                            NotificationManager.error('Error occurred while making your changes, contact support!', 'Error!');
+                        });
+         }
     updateBalance() {
         this.state.data.balance = this.state.balance;
         console.log(this.state);
@@ -51,9 +101,11 @@ class WalletInfo extends Component {
         })
             .then(response => response.json())
             .then(data => {
+                console.log('Success:', data);
                 NotificationManager.success('You changes have been updated!', 'Successful!', 1000);
             })
             .catch((error) => {
+                console.error('Error:', error);
                 NotificationManager.error('Error occurred while making your changes, contact support!', 'Error!');
             });
     }
@@ -112,6 +164,65 @@ class WalletInfo extends Component {
                         </TableRow>
                     </TableBody>
                 </Table>
+                <Divider/>
+                <Typography component="h2" variant="h6" style={{color: 'blue',marginTop:20}} align={"center"} gutterBottom>
+                    Wallet Update
+                </Typography>
+                <Table>
+                    <TableBody>
+                    <TableRow>
+                            <TableCell style={{borderBottom: "none"}}>
+                                <FormLabel style={{color: 'indianred'}}>
+                                  Enter Amount
+                                </FormLabel>
+                                <TextField
+                                    fullWidth
+                                    id="standard-bare"
+                                    name = "amount"
+                                    variant="outlined"
+                                    defaultValue={this.state.data.balance}
+                                    value={this.state.wallet.amount}
+                                    onChange={evt => this.handleChangeWallet(evt)}
+                                />
+                            </TableCell>
+                            </TableRow>
+                            <TableRow>
+                            <TableCell style={{borderBottom: "none"}}>
+                                <FormControl fullWidth>
+                            <FormLabel style={{color: 'indianred'}}>
+                                  Select Withdraw/Deposit
+                                </FormLabel>
+                            <Select name="type" value='withdraw' onChange={this.handleChangeWallet}
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    label="transaction"
+                                    >
+                                    <MenuItem value="withdraw">Withdraw</MenuItem>
+                                    <MenuItem value="deposit">Deposit</MenuItem>
+                                    </Select>
+                                    </FormControl>
+                                    </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                <TextField variant="outlined" fullWidth name="description"
+                                        id="outlined-multiline-static"
+                                        label="Enter Description"
+                                        multiline
+                                        onChange={(event)=>this.handleChangeDescription(event)}
+                                        rows={4}
+                                        defaultValue="I would like to Withdraw/Deposit xxx for..."
+                                        />
+                         <Table>
+                             <TableCell style={{display:'flex',justifyContent:'center'}}>
+                                <Button color="primary"
+                                    variant="contained"
+                                    onClick={this.proceedTransaction}
+                                >Proceed</Button>
+                                </TableCell>
+                                </Table>
+                                    </TableRow>
+                        </TableBody>
+                        </Table>
             </div>
         )
     }
